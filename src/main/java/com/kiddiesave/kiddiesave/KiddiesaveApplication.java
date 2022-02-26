d6807df1360e337e6b1b085d4df2dea4aefae504
Ayorinde Adunse <ayorindeadunse@Ayorindes-MacBook-Air.local>
2022-02-26 11:50:25 +0100

    initialized kiddiesave v1 project

---
+++ src/main/java/com/kiddiesave/kiddiesave/KiddiesaveApplication.java
@@ -0,0 +1,13 @@
+package com.kiddiesave.kiddiesave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiddiesaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiddiesaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiddiesave/kiddiesave/KiddiesaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiddiesave.kiddiesave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiddiesaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
