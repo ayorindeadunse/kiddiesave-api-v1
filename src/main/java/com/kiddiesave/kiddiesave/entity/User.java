438c5c3540c7d928c215c3081a97bfb66bd77f95
Ayorinde Adunse <ayorindeadunse@Ayorindes-MacBook-Air.local>
2022-03-01 19:21:34 +0100

    Updated User model

--- src/main/java/com/kiddiesave/kiddiesave/entity/User.java
+++ src/main/java/com/kiddiesave/kiddiesave/entity/User.java
@@ -1,4 +1,27 @@
 package com.kiddiesave.kiddiesave.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class User {
+    @Id // Marks the Id field as primary key and therefore the identifier of this entity.
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
 }
