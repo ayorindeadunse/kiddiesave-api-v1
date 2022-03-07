f9f4558d389443fd0a9f2d68e5535a170698aee0
Ayorinde Adunse <ayorindeadunse@Ayorindes-MacBook-Air.local>
2022-03-07 13:27:41 +0100

    Added SecurityConfigurer.java class

--- src/main/java/com/kiddiesave/kiddiesave/configuration/SwaggerConfig.java
+++ src/main/java/com/kiddiesave/kiddiesave/configuration/SwaggerConfig.java
@@ -1,4 +1,64 @@
 package com.kiddiesave.kiddiesave.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
 public class SwaggerConfig {
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    private ApiInfo apiInfo()
+    {
+        return new ApiInfo("Kiddiesave REST API Implementation",
+        "APIs for Kiddiesave.",
+        "1.0",
+        "Terms of service",
+        new Contact("Kiddiesave API","https://www.linkedin.com/in/ayorindeadunse","adunseayorinde@gmail.com"),//replace with an actual url
+        "API License",
+        "API License URL",
+                Collections.emptyList());
+    }
+
+    @Bean
+    public Docket api()
+    {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                //add security contexts
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    // Api Key
+    private ApiKey apiKey()
+    {
+        return new ApiKey(AUTHORIZATION_HEADER,"JWT","header");
+    }
+
+    private SecurityContext securityContext()
+    {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+    List<SecurityReference> defaultAuth()
+    {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global","accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference(AUTHORIZATION_HEADER,authorizationScopes));
+    }
 }
