eb7b239d411844a4434a78fd68984e810d7fddb2
Ayorinde Adunse <ayorindeadunse@Ayorindes-MacBook-Air.local>
2022-03-12 00:51:49 +0100

    added EmailModel

---
+++ src/main/java/com/kiddiesave/kiddiesave/entity/EmailModel.java
@@ -0,0 +1,22 @@
+package com.kiddiesave.kiddiesave.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class EmailModel {
+    private String from;
+    private String  to;
+    private String subject;
+    private String message;
+    private String mailCC;
+    private String mailBcc;
+    private String contentType;
+    private String attachment;
+
+}
