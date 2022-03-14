13c7e47328137fb3312368aa9f15a29e2b17f12b
Ayorinde Adunse <ayorindeadunse@Ayorindes-MacBook-Air.local>
2022-03-14 13:44:58 +0100

    Added Exceptions package to project and UserNotFoundException class

---
+++ src/main/java/com/kiddiesave/kiddiesave/exceptions/UserNotFoundException.java
@@ -0,0 +1,28 @@
+package com.kiddiesave.kiddiesave.exceptions;
+
+public class UserNotFoundException extends Exception{
+    private Long userid;
+    private String email;
+    private String mobile;
+
+
+    public UserNotFoundException(Long userid) {
+        super(String.format("The user with ", userid, "does not exist : '%s'"
+        ));
+    }
+
+    public UserNotFoundException(String email) {
+        super(String.format("The user with ",email," does not exist : '%s'"));
+        this.email = email;
+    }
+
+    public UserNotFoundException(String message, String mobile) {
+        super(String.format("The user with ",mobile, " does not exist : '%s'"+ message));
+        this.mobile = mobile;
+    }
+
+    public String UserNotFoundException(String message)
+    {
+        return String.format(message);
+    }
+}
