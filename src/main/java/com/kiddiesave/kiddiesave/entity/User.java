b9f45681014207cb83a22740e5958f724cd3fc2c
Ayorinde Adunse <ayorindeadunse@Ayorindes-MacBook-Air.local>
2022-03-15 09:29:16 +0100

    updated User entity class

--- src/main/java/com/kiddiesave/kiddiesave/entity/User.java
+++ src/main/java/com/kiddiesave/kiddiesave/entity/User.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
 @Getter
@@ -21,8 +22,36 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String email;
-
     //update with the rest of user properties.
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
+    private String bvn;
+    private String title;
+    private String nin;
+    private String phoneNumberLinkedWithBvn;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private Date dob;
+    private String gender;
+    private UserType userType;
+    private String notified;
+    private int accessFailedCount;
+    private boolean lockedOutEnabled;
+    private String address;
+    private String referralName;
+    private String referralPhoneNo;
+    private String dateCreated;
+    private String dateUpdated;
+    private String deviceId;
+    private String refreshToken;
+    private boolean isBvnValidated;
+    private String isPhoneValidated;
+    private boolean isPushNotifications; //push notifications enabled or not
+    private String NotificationId;
+    private String pinHash;
+    private String pinSalt;
+    private String status;
+
+
 }
