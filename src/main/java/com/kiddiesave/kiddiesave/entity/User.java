7c034c79af68ca5df64ff57e8ed3e417f2792ecf
Ayorinde Adunse <ayorindeadunse@Ayorindes-MacBook-Air.local>
2022-03-19 17:47:58 +0100

    updated User.java

--- src/main/java/com/kiddiesave/kiddiesave/entity/User.java
+++ src/main/java/com/kiddiesave/kiddiesave/entity/User.java
@@ -1,15 +1,20 @@
 package com.kiddiesave.kiddiesave.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 
 @Entity
@@ -17,7 +22,9 @@ import java.util.Date;
 @Setter
 @ToString
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
+    // Possibly refacotr this User class to implement/extend UserDetails so you can make use of Spring Security
+    // features
     @Id // Marks the Id field as primary key and therefore the identifier of this entity.
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -37,7 +44,7 @@ public class User {
     private UserType userType;
     private String notified;
     private int accessFailedCount;
-    private boolean lockedOutEnabled; // use the methods provided for in UserDetails class for this implementation
+   // private boolean lockedOutEnabled; // use the methods provided for in UserDetails class for this implementation
     private String address;
     private String referralName;
     private String referralPhoneNo;
@@ -54,4 +61,43 @@ public class User {
     private String status;
 
 
+    @JsonIgnore
+    @Override
+    public Collection<GrantedAuthority> getAuthorities() {
+        return new ArrayList<>();
+    }
+
+    @JsonIgnore
+    @Override
+    public String getUsername()
+    {
+        return email;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonExpired()
+    {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonLocked()
+    {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isCredentialsNonExpired()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled()
+    {
+        return true;
+    }
 }
