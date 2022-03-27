a4e674cd12d326740b2a9e16cfe59339bb86dc87
ayorindeadunse <Ayorinde.Adunse@azenia.com>
2022-03-27 09:39:11 +0100

    updated UserDetailsImpl class

--- src/main/java/com/kiddiesave/kiddiesave/security/UserDetailsImpl.java
+++ src/main/java/com/kiddiesave/kiddiesave/security/UserDetailsImpl.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class UserDetailsImpl implements UserDetails {
@@ -39,5 +40,65 @@ public class UserDetailsImpl implements UserDetails {
                 user.getPassword(),
                 authorities
         );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities()
+    {
+        return authorities;
+    }
+
+    public Long getId()
+    {
+        return id;
+    }
+    public String getEmail()
+    {
+        return email;
+    }
+
+    @Override
+    public String getUsername()
+    {
+        return email;
+    }
+    @Override
+    public String getPassword()
+    {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonLocked()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonExpired()
+    {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if(this == o)
+            return true;
+        if(o == null || getClass() != o.getClass())
+            return false;
+        UserDetailsImpl user = (UserDetailsImpl) o;
+        return Objects.equals(id,user.id);
     }
 }
