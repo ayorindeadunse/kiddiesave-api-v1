7ea29aa24f81844265f985385302672703282e90
ayorindeadunse <adunseayorinde@gmail.com>
2022-04-15 15:05:29 +0100

    Added ClaimsResponse utility class

---
+++ src/main/java/com/kiddiesave/kiddiesave/RequestsAndResponses/ClaimsResponse.java
@@ -0,0 +1,27 @@
+package com.kiddiesave.kiddiesave.RequestsAndResponses;
+
+import com.kiddiesave.kiddiesave.entity.Role;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+
+public class ClaimsResponse {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String address;
+    private Set<Role> roles;
+    private String mobile;
+    private String bvn;
+    private String gender;
+    private Date dob;
+}
--- src/main/java/com/kiddiesave/kiddiesave/security/util/Claims.java
+++ src/main/java/com/kiddiesave/kiddiesave/security/util/Claims.java
@@ -1,5 +1,8 @@
 package com.kiddiesave.kiddiesave.security.util;
 
+import com.kiddiesave.kiddiesave.entity.User;
+import com.kiddiesave.kiddiesave.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,5 +11,8 @@ public class Claims {
     private String username;
     private String bearerToken;
 
+    @Autowired
+    private UserService userService;
+    private User user;
 
 }
