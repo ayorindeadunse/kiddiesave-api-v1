c408827f447851f06d5935013254ce2bf97db8b3
ayorindeadunse <adunseayorinde@gmail.com>
2022-05-06 01:25:00 +0100

    Added ValidationController

--- src/main/java/com/kiddiesave/kiddiesave/controllers/ValidationController.java
+++ src/main/java/com/kiddiesave/kiddiesave/controllers/ValidationController.java
@@ -1,4 +1,28 @@
 package com.kiddiesave.kiddiesave.controllers;
 
+import com.kiddiesave.kiddiesave.RequestsAndResponses.ValidatePhoneNumberRequest;
+import com.kiddiesave.kiddiesave.services.ValidationServiceRestImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/validation")
 public class ValidationController {
+
+    private final ValidationServiceRestImpl validationServiceRest;
+
+    public ValidationController(ValidationServiceRestImpl validationServiceRest) {
+        this.validationServiceRest = validationServiceRest;
+    }
+
+    @PostMapping("/generateotp")
+    public ResponseEntity<?> ValidatePhoneNumber(@Valid @RequestBody ValidatePhoneNumberRequest validatePhoneNumberRequest) {
+
+        return null;
+    }
 }
--- src/main/java/com/kiddiesave/kiddiesave/services/ValidationServiceRestImpl.java
+++ src/main/java/com/kiddiesave/kiddiesave/services/ValidationServiceRestImpl.java
@@ -3,6 +3,7 @@ package com.kiddiesave.kiddiesave.services;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kiddiesave.kiddiesave.RequestsAndResponses.ValidatePhoneNumber;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,6 +14,7 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 // test implementation
+@Service
 public class ValidationServiceRestImpl implements ValidationServiceRest{
 
     @Value("${SmsOtpUrl}")
