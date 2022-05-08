341cae19e60b9745b8bf337f186bf29e0b7b36ed
ayorindeadunse <adunseayorinde@gmail.com>
2022-05-08 08:33:40 +0100

    Temporarily Removed ValidationRepository

--- src/main/java/com/kiddiesave/kiddiesave/repository/ValidationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kiddiesave.kiddiesave.repository;
-
-import com.kiddiesave.kiddiesave.RequestsAndResponses.ValidatePhoneNumberRequest;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ValidationRepository extends CrudRepository<ValidatePhoneNumberRequest,Long> {
-
-}
