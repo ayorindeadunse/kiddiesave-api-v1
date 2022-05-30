d00ac77d4a84b7e25b5106a34d767a4169218f4e
ayorindeadunse <adunseayorinde@gmail.com>
2022-05-30 20:39:53 +0100

    updated ValidatePhoneNumber controller

--- src/main/java/com/kiddiesave/kiddiesave/RequestsAndResponses/ValidatePhoneNumberRequest.java
+++ src/main/java/com/kiddiesave/kiddiesave/RequestsAndResponses/ValidatePhoneNumberRequest.java
@@ -3,12 +3,14 @@ package com.kiddiesave.kiddiesave.RequestsAndResponses;
 import lombok.Getter;
 import lombok.Setter;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.util.Date;
 
 @Getter
 @Setter
 public class ValidatePhoneNumberRequest {
     @NotBlank
+    @Pattern(regexp = "^234\\d{10}",message = "The minimum length of the mobile number should be 13 and start with 234.")
     private String phone;
    // private String code;
   //  private String pinId;
--- src/main/java/com/kiddiesave/kiddiesave/controllers/ValidationController.java
+++ src/main/java/com/kiddiesave/kiddiesave/controllers/ValidationController.java
@@ -1,10 +1,13 @@
 package com.kiddiesave.kiddiesave.controllers;
 
+import com.kiddiesave.kiddiesave.RequestsAndResponses.ApiResponse;
 import com.kiddiesave.kiddiesave.RequestsAndResponses.ValidatePhoneNumberRequest;
 import com.kiddiesave.kiddiesave.RequestsAndResponses.ValidatePhoneNumberResponse;
+import com.kiddiesave.kiddiesave.services.PhoneValidationServiceImpl;
 import com.kiddiesave.kiddiesave.services.ValidationServiceRestImpl;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,17 +21,27 @@ import java.io.IOException;
 @RequestMapping("/api/validation")
 public class ValidationController {
 
-    private final ValidationServiceRestImpl validationServiceRest;
+    private final PhoneValidationServiceImpl phoneValidationService;
 
-    public ValidationController(ValidationServiceRestImpl validationServiceRest) {
-        this.validationServiceRest = validationServiceRest;
+    public ValidationController(PhoneValidationServiceImpl phoneValidationService) {
+        this.phoneValidationService = phoneValidationService;
     }
 
-    @PostMapping("/generateotp")
+    //Allow anonymous
+    @PostMapping("/phone")
     public ResponseEntity<?> ValidatePhoneNumber(@Valid @RequestBody ValidatePhoneNumberRequest validatePhoneNumberRequest)
             throws UnirestException {
 
-        ValidatePhoneNumberResponse otp = validationServiceRest.sendOTPCode(validatePhoneNumberRequest.getPhone());
-       return ResponseEntity.ok(otp);
+            // Mobile number validation already handled by regex in model
+            String pin = phoneValidationService.generatePhoneValidationCode(validatePhoneNumberRequest);
+            if(pin != null) {
+                return ResponseEntity.ok(new ApiResponse(true, "Otp sent successfully!", pin));
+            }
+            else
+            {
+                return new ResponseEntity(new ApiResponse(false, "An Error occurred on the server!", HttpStatus.INTERNAL_SERVER_ERROR),
+                        HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+
     }
 }
