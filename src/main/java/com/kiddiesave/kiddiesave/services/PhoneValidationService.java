0323b5f690088ae3c70c0adc47bb42ec1357088c
ayorindeadunse <adunseayorinde@gmail.com>
2022-05-30 19:57:42 +0100

    implemented PhoneValidationServiceImpl

--- src/main/java/com/kiddiesave/kiddiesave/services/PhoneValidationService.java
+++ src/main/java/com/kiddiesave/kiddiesave/services/PhoneValidationService.java
@@ -2,9 +2,10 @@ package com.kiddiesave.kiddiesave.services;
 
 import com.kiddiesave.kiddiesave.RequestsAndResponses.ValidatePhoneNumberRequest;
 import com.kiddiesave.kiddiesave.entity.PhoneRecord;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 public interface PhoneValidationService {
-    String generatePhoneValidationCode(ValidatePhoneNumberRequest request);
+    String generatePhoneValidationCode(ValidatePhoneNumberRequest request) throws UnirestException;
     // save phone number
     PhoneRecord savePhoneNumber(String phone);
 }
--- src/main/java/com/kiddiesave/kiddiesave/services/PhoneValidationServiceImpl.java
+++ src/main/java/com/kiddiesave/kiddiesave/services/PhoneValidationServiceImpl.java
@@ -1,17 +1,41 @@
 package com.kiddiesave.kiddiesave.services;
 
 import com.kiddiesave.kiddiesave.RequestsAndResponses.ValidatePhoneNumberRequest;
+import com.kiddiesave.kiddiesave.RequestsAndResponses.ValidatePhoneNumberResponse;
 import com.kiddiesave.kiddiesave.entity.PhoneRecord;
+import com.kiddiesave.kiddiesave.repository.PhoneNumberRepository;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
+@Service
 public class PhoneValidationServiceImpl implements PhoneValidationService {
 
+    @Autowired
+    PhoneNumberRepository phoneNumberRepository;
+    @Autowired
+    ValidationServiceRestImpl validationServiceRest;
+
+    public PhoneValidationServiceImpl(PhoneNumberRepository phoneNumberRepository,ValidationServiceRestImpl validationServiceRest) {
+        this.phoneNumberRepository = phoneNumberRepository;
+        this.validationServiceRest = validationServiceRest;
+    }
+
     @Override
-    public String generatePhoneValidationCode(ValidatePhoneNumberRequest request) {
-        return null;
+    public String generatePhoneValidationCode(ValidatePhoneNumberRequest request) throws UnirestException {
+        ValidatePhoneNumberResponse validatePhoneNumberResponse = validationServiceRest.sendOTPCode(request.getPhone());
+        return validatePhoneNumberResponse.getPinId();
     }
 
     @Override
     public PhoneRecord savePhoneNumber(String phone) {
-        return null;
+        PhoneRecord pr = new PhoneRecord();
+        pr.setPhone(phone);
+        pr.setDateCreated(new Date());
+
+       PhoneRecord savePhoneNumber  = phoneNumberRepository.save(pr);
+       return savePhoneNumber;
     }
 }
