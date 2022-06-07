afc520502529f4e0182edcb6804d3201d9948422
= <=>
2022-06-07 16:06:15 +0100

    updated ValidatePhoneNumberRequest DTO

--- src/main/java/com/kiddiesave/kiddiesave/RequestsAndResponses/ValidatePhoneNumberRequest.java
+++ src/main/java/com/kiddiesave/kiddiesave/RequestsAndResponses/ValidatePhoneNumberRequest.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
-import java.util.Date;
 
 @Getter
 @Setter
@@ -12,7 +11,4 @@ public class ValidatePhoneNumberRequest {
     @NotBlank
     @Pattern(regexp = "^234\\d{10}",message = "The minimum length of the mobile number should be 13 and start with 234.")
     private String phone;
-   // private String code;
-  //  private String pinId;
-   // private Date dateCreated;
 }
