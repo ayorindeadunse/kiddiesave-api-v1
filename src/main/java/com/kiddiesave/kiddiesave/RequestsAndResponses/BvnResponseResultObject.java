934d40f95aa0a7eda5cfcbe6781956860880d4b8
= <adunseayorinde@gmail.com>
2022-06-07 16:31:28 +0100

    Added BvnResponseResultObject pojo

--- src/main/java/com/kiddiesave/kiddiesave/RequestsAndResponses/BvnResponseResultObject.java
+++ src/main/java/com/kiddiesave/kiddiesave/RequestsAndResponses/BvnResponseResultObject.java
@@ -1,4 +1,19 @@
 package com.kiddiesave.kiddiesave.RequestsAndResponses;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class BvnResponseResultObject {
+    private String bvn;
+    private String phoneNumber;
+    private String firstName;
+    private String lastName;
+    private String otherNames;
+    private String dob;
 }
