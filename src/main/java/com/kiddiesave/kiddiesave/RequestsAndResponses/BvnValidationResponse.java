e1593b332125e5759559f85c7dee469b2b86bc4e
= <adunseayorinde@gmail.com>
2022-06-11 21:30:45 +0100

    updated pojo BvnValidationResponse

--- src/main/java/com/kiddiesave/kiddiesave/RequestsAndResponses/BvnValidationResponse.java
+++ src/main/java/com/kiddiesave/kiddiesave/RequestsAndResponses/BvnValidationResponse.java
@@ -1,11 +1,15 @@
 package com.kiddiesave.kiddiesave.RequestsAndResponses;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 
 @Getter
 @Setter
+@NoArgsConstructor
+@ToString
 
 public class BvnValidationResponse {
 
