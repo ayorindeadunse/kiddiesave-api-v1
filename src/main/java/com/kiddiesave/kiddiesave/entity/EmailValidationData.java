cda92fdaf3795d1bef9addb553660a72136f06fa
adunseayorinde <adunseayorinde@gmail.com>
2022-06-23 08:50:13 +0100

    Added EmailValidationData entity

---
+++ src/main/java/com/kiddiesave/kiddiesave/entity/EmailValidationData.java
@@ -0,0 +1,29 @@
+package com.kiddiesave.kiddiesave.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+
+@Entity
+public class EmailValidationData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String requestId;
+    private String email;
+    private Date dateCreated;
+
+}
