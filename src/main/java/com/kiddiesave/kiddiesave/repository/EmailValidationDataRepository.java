4cbbbd2fb4fa269931a80d0736582b889aed966c
adunseayorinde <adunseayorinde@gmail.com>
2022-06-23 08:51:41 +0100

    Added EmailValidationDataRepository

---
+++ src/main/java/com/kiddiesave/kiddiesave/repository/EmailValidationDataRepository.java
@@ -0,0 +1,9 @@
+package com.kiddiesave.kiddiesave.repository;
+
+import com.kiddiesave.kiddiesave.entity.EmailValidationData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmailValidationDataRepository extends JpaRepository<EmailValidationData,Long> {
+}
