98697145a89cfa0e107054a4ba45310e2d3d0037
adunseayorinde <adunseayorinde@gmail.com>
2022-06-26 09:24:50 +0100

    Added body of method sendValidationEmail

--- src/main/java/com/kiddiesave/kiddiesave/services/ValidateEmailServiceImpl.java
+++ src/main/java/com/kiddiesave/kiddiesave/services/ValidateEmailServiceImpl.java
@@ -2,16 +2,22 @@ package com.kiddiesave.kiddiesave.services;
 
 import com.kiddiesave.kiddiesave.RequestsAndResponses.ValidateEmailRequest;
 import com.kiddiesave.kiddiesave.entity.EmailValidationData;
+import com.kiddiesave.kiddiesave.entity.User;
+import com.kiddiesave.kiddiesave.exceptions.UserNotFoundException;
 import com.kiddiesave.kiddiesave.repository.EmailValidationDataRepository;
+import com.kiddiesave.kiddiesave.repository.UserRepository;
 
 import java.util.Date;
+import java.util.UUID;
 
 public class ValidateEmailServiceImpl implements ValidateEmailService{
 
     private final EmailValidationDataRepository emailValidationDataRepository;
+    private final UserRepository userRepository;
 
-    public ValidateEmailServiceImpl(EmailValidationDataRepository emailValidationDataRepository) {
+    public ValidateEmailServiceImpl(EmailValidationDataRepository emailValidationDataRepository, UserRepository userRepository) {
         this.emailValidationDataRepository = emailValidationDataRepository;
+        this.userRepository = userRepository;
     }
 
 
@@ -28,6 +34,29 @@ public class ValidateEmailServiceImpl implements ValidateEmailService{
 
     @Override
     public String sendValidationEmail(ValidateEmailRequest request) {
+        try
+        {
+            // get user
+            User u = userRepository.getUserByEmail(request.getEmail());
+            if(u == null || !u.isStatus())
+            {
+                throw new UserNotFoundException("User does not exist");
+            }
+            UUID uuid = UUID.randomUUID();
+            String requestId = uuid.toString();
+            /**send validation email
+             * *
+             */
+            // save data in database
+            request.setRequestId(requestId);
+            saveEmailValidationRequest(request);
+            return "Email successfully sent to user";
+        }
+        // catch custom validation exception here
+        catch(Exception e)
+        {
+            System.out.println("Failure to send validation e-mail "  + e);
+        }
         return null;
     }
 }
