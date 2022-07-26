b5c2a42316e82de8ee8c1497a0ebbe864dfd41ef
adunseayorinde <adunseayorinde@gmail.com>
2022-07-26 19:11:18 +0100

    Adding EmailServiceImpl body

--- src/main/java/com/kiddiesave/kiddiesave/services/EmailServiceImpl.java
+++ src/main/java/com/kiddiesave/kiddiesave/services/EmailServiceImpl.java
@@ -1,10 +1,20 @@
 package com.kiddiesave.kiddiesave.services;
 
 import com.kiddiesave.kiddiesave.models.EmailModel;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailServiceImpl implements EmailService{
+    JavaMailSender mailSender;
+    private Environment env;
+
+    public EmailServiceImpl(JavaMailSender mailSender, Environment env) {
+        this.mailSender = mailSender;
+        this.env = env;
+    }
+
     @Override
     public void sendEmail(EmailModel emailModel) {
 
