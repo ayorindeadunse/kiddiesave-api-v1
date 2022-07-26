ff8a31ebac4b0b4d9d69e05964a18582f127a9eb
adunseayorinde <adunseayorinde@gmail.com>
2022-07-26 17:57:31 +0100

    added java mail dependencies

---
+++ src/main/java/com/kiddiesave/kiddiesave/services/EmailServiceImpl.java
@@ -0,0 +1,17 @@
+package com.kiddiesave.kiddiesave.services;
+
+import com.kiddiesave.kiddiesave.models.EmailModel;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService{
+    @Override
+    public void sendEmail(EmailModel emailModel) {
+
+    }
+
+    @Override
+    public void sendEmailWithAttachment(EmailModel emailModel) {
+
+    }
+}
