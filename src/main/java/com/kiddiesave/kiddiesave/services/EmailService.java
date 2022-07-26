e5174683687ff023dc2e0f0df8cabdbe52c9c646
adunseayorinde <adunseayorinde@gmail.com>
2022-07-26 19:31:21 +0100

    Added body of sendEmailWithAttachment method

--- src/main/java/com/kiddiesave/kiddiesave/services/EmailService.java
+++ src/main/java/com/kiddiesave/kiddiesave/services/EmailService.java
@@ -2,7 +2,9 @@ package com.kiddiesave.kiddiesave.services;
 
 import com.kiddiesave.kiddiesave.models.EmailModel;
 
+import javax.mail.MessagingException;
+
 public interface EmailService {
-    void sendEmail(EmailModel emailModel);
-    void sendEmailWithAttachment(EmailModel emailModel);
+    void sendEmail(EmailModel emailModel) throws MessagingException;
+    void sendEmailWithAttachment(EmailModel emailModel) throws MessagingException;
 }
--- src/main/java/com/kiddiesave/kiddiesave/services/EmailServiceImpl.java
+++ src/main/java/com/kiddiesave/kiddiesave/services/EmailServiceImpl.java
@@ -2,8 +2,13 @@ package com.kiddiesave.kiddiesave.services;
 
 import com.kiddiesave.kiddiesave.models.EmailModel;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 @Service
 public class EmailServiceImpl implements EmailService{
@@ -21,7 +26,16 @@ public class EmailServiceImpl implements EmailService{
     }
 
     @Override
-    public void sendEmailWithAttachment(EmailModel emailModel) {
-
+    public void sendEmailWithAttachment(EmailModel emailModel) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message,true);
+        helper.setSubject(emailModel.getSubject());
+        helper.setFrom(emailModel.getFrom());
+        helper.setTo(emailModel.getTo());
+        helper.setText(emailModel.getMessage());
+        FileSystemResource file = new FileSystemResource(new File(emailModel.getAttachment()));
+        helper.addAttachment("Attachment",file);
+        mailSender.send(helper.getMimeMessage());
+        System.out.println("Email Sent Successfully"); //replace with logger
     }
 }
