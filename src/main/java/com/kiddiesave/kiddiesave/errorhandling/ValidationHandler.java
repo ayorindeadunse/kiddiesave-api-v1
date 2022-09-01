7fa697f3b0368695ae2335e2d8a61377953b3844
adunseayorinde <adunseayorinde@gmail.com>
2022-09-01 18:59:59 +0100

    added ValidationHandler

---
+++ src/main/java/com/kiddiesave/kiddiesave/errorhandling/ValidationHandler.java
@@ -0,0 +1,6 @@
+package com.kiddiesave.kiddiesave.errorhandling;
+
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class ValidationHandler extends ResponseEntityExceptionHandler {
+}
