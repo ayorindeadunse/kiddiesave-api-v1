36592e68bd6e825da417bf279cf0a0681b6e2ab9
adunseayorinde <adunseayorinde@gmail.com>
2022-09-23 17:06:58 +0100

    updated UserService

--- src/main/java/com/kiddiesave/kiddiesave/services/UserService.java
+++ src/main/java/com/kiddiesave/kiddiesave/services/UserService.java
@@ -6,7 +6,6 @@ import com.kiddiesave.kiddiesave.RequestsAndResponses.UpdateUserRequest;
 import com.kiddiesave.kiddiesave.entity.User;
 import com.kiddiesave.kiddiesave.exceptions.ApplicationException;
 import com.kiddiesave.kiddiesave.exceptions.UserNotFoundException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 
 public interface UserService {
