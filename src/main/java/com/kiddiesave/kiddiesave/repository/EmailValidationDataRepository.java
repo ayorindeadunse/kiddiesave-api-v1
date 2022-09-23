b74622b657a50b9a63d97f2785eef5b4a55cfb7a
adunseayorinde <adunseayorinde@gmail.com>
2022-09-23 16:59:41 +0100

    updated EmailValidationDataRepository

--- src/main/java/com/kiddiesave/kiddiesave/repository/EmailValidationDataRepository.java
+++ src/main/java/com/kiddiesave/kiddiesave/repository/EmailValidationDataRepository.java
@@ -1,6 +1,5 @@
 package com.kiddiesave.kiddiesave.repository;
 
-import com.kiddiesave.kiddiesave.RequestsAndResponses.ValidateEmailRequest;
 import com.kiddiesave.kiddiesave.entity.EmailValidationData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
