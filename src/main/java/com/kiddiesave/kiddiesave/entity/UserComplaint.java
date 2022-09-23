8b7860ecca7a94c79dd9a97756802ec06049b412
adunseayorinde <adunseayorinde@gmail.com>
2022-09-23 16:57:10 +0100

    Updated UserComplaint class

--- src/main/java/com/kiddiesave/kiddiesave/entity/UserComplaint.java
+++ src/main/java/com/kiddiesave/kiddiesave/entity/UserComplaint.java
@@ -1,10 +1,5 @@
 package com.kiddiesave.kiddiesave.entity;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
 import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
