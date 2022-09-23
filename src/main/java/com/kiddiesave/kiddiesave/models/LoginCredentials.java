751c16e51aaa19131b1029ebfba31b84c84e2d4d
adunseayorinde <adunseayorinde@gmail.com>
2022-09-23 16:58:52 +0100

    updated LoginCredentials pojo

--- src/main/java/com/kiddiesave/kiddiesave/models/LoginCredentials.java
+++ src/main/java/com/kiddiesave/kiddiesave/models/LoginCredentials.java
@@ -2,11 +2,11 @@ package com.kiddiesave.kiddiesave.models;
 
 import lombok.*;
 
-@Getter // Defines the getter methods of the member fields
-@Setter // Defines the setter methods of the member fields
-@AllArgsConstructor // Defines a constructor that initializes all the member fields
-@NoArgsConstructor // Defines a no-args default constructor
-@ToString // Defines a meaningful toString implementation of this class
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class LoginCredentials {
     private String email;
     private String password;
