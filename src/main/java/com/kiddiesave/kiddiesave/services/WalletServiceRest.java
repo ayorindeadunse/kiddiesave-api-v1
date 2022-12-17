f8a6a92467bd408869ccdb778e756a1b93ca534f
adunseayorinde <adunseayorinde@gmail.com>
2022-12-17 14:36:52 +0100

    updated WalletServiceRestImpl

--- src/main/java/com/kiddiesave/kiddiesave/services/WalletServiceRest.java
+++ src/main/java/com/kiddiesave/kiddiesave/services/WalletServiceRest.java
@@ -1,9 +1,10 @@
 package com.kiddiesave.kiddiesave.services;
 
 import com.kiddiesave.kiddiesave.RequestsAndResponses.CreateWalletResponse;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 import java.io.IOException;
 
 public interface WalletServiceRest {
-    CreateWalletResponse createWallet(String email) throws IOException;
+    CreateWalletResponse createWallet(String email) throws UnirestException;
 }
--- src/main/java/com/kiddiesave/kiddiesave/services/WalletServiceRestImpl.java
+++ src/main/java/com/kiddiesave/kiddiesave/services/WalletServiceRestImpl.java
@@ -1,12 +1,28 @@
 package com.kiddiesave.kiddiesave.services;
 
 import com.kiddiesave.kiddiesave.RequestsAndResponses.CreateWalletResponse;
+import com.kiddiesave.kiddiesave.security.util.LocalDateAdapter;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.io.IOException;
 
 public class WalletServiceRestImpl implements WalletServiceRest{
+
+    @Value("${get_wallets_secret_key}")
+    private String getWalletsSecretKey;
+    @Value("${get_wallets_base_url}")
+    private String getWalletsBaseUrl;
+
+    private final LocalDateAdapter localDateAdapter;
+
+    @Autowired
+    public WalletServiceRestImpl(LocalDateAdapter localDateAdapter) {
+        this.localDateAdapter = localDateAdapter;
+    }
     @Override
-    public CreateWalletResponse createWallet(String email) throws IOException {
-        return null;
+    public CreateWalletResponse createWallet(String email) throws UnirestException {
+    return null;
     }
 }
