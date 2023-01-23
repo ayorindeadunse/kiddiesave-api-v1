76e412080df8174d05374de31ea33453a2d70581
adunseayorinde <adunseayorinde@gmail.com>
2023-01-23 16:10:04 +0100

    Added WalletService contract and class to implement methods

--- src/main/java/com/kiddiesave/kiddiesave/services/WalletService.java
+++ src/main/java/com/kiddiesave/kiddiesave/services/WalletService.java
@@ -1,4 +1,9 @@
 package com.kiddiesave.kiddiesave.services;
 
+import okhttp3.Response;
+
+import java.io.IOException;
+
 public interface WalletService {
+    Response createWallet(String email) throws IOException;
 }
--- src/main/java/com/kiddiesave/kiddiesave/services/WalletServiceImpl.java
+++ src/main/java/com/kiddiesave/kiddiesave/services/WalletServiceImpl.java
@@ -1,4 +1,13 @@
 package com.kiddiesave.kiddiesave.services;
 
-public class WalletServiceImpl {
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class WalletServiceImpl implements WalletService{
+
+    @Override
+    public Response createWallet(String email) throws IOException {
+        return null;
+    }
 }
