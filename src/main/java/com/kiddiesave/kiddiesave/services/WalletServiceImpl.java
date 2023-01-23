e70229d919a71b00938b7ffae2dd41cdfa0ae9bb
adunseayorinde <adunseayorinde@gmail.com>
2023-01-23 16:14:32 +0100

    Added implementation of CreateWallet method

--- src/main/java/com/kiddiesave/kiddiesave/services/WalletServiceImpl.java
+++ src/main/java/com/kiddiesave/kiddiesave/services/WalletServiceImpl.java
@@ -1,13 +1,22 @@
 package com.kiddiesave.kiddiesave.services;
 
 import okhttp3.Response;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
 
 public class WalletServiceImpl implements WalletService{
 
+    private final WalletServiceRestImpl walletServiceRest;
+
+    @Autowired
+    public WalletServiceImpl(WalletServiceRestImpl walletServiceRest) {
+        this.walletServiceRest = walletServiceRest;
+    }
+
     @Override
     public Response createWallet(String email) throws IOException {
-        return null;
+        Response createUserWallet = walletServiceRest.createWallet(email);
+        return createUserWallet;
     }
 }
